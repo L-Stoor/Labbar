6c29ae52147a63018900955975114a38e95beda9
L-Stoor <143792234+L-Stoor@users.noreply.github.com>
2023-10-13 11:11:01 +0200

    Labb 6.py

--- Labb 6.py
+++ Labb 6.py
@@ -1,15 +1,16 @@
-import hanteralabb
 """ Importerar vår felhanterings kod som hanterar vår input i koden """
+import hanteralabb
 
 
 def läsa_fil():
-    """ Skapar en funktion som ska öppna och läsa innehållet i en fil och lägga in informationen i en lista """
+    """ Skapar en funktion som ska öppna och läsa innehållet 
+    i en fil och lägga in informationen i en lista """
 
     hanteralabb.lista("Vad heter filen? ")
     fil = open("students.txt", "r", encoding = "utf-8") # utf-8 gör att python kan läsa ÅÄÖ
     lista = []
 
-    for n in range(6): # Det är antalet studenter som finns i listan "students.txt"
+    for n in range(5): # Det är antalet studenter som finns i listan "students.txt"
 
         personnummer = fil.readline().strip()
         namn = fil.readline().strip()
