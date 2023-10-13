bf48582ff26dee2e56c882b5d71a163bfe29e483
L-Stoor <143792234+L-Stoor@users.noreply.github.com>
2023-10-13 10:41:07 +0200

    Add files via upload

---
+++ Labb 6.py
@@ -0,0 +1,23 @@
+import hanteralabb
+""" Importerar vår felhanterings kod som hanterar vår input i koden """
+
+
+def läsa_fil():
+    """ Skapar en funktion som ska öppna och läsa innehållet i en fil och lägga in informationen i en lista """
+
+    inmatning = hanteralabb.lista("Vad heter filen? ")
+    fil = open("students.txt", "r", encoding = "utf-8") # utf-8 gör att python kan läsa ÅÄÖ
+    lista = []
+
+    for n in range(6): # Det är antalet studenter som finns i listan "students.txt"
+
+        personnummer = fil.readline().strip()
+        namn = fil.readline().strip()
+        efternamn = fil.readline().strip()
+        lista.append([efternamn, namn, personnummer])
+
+    print("Dessa studenter är skrivna på KTH")
+    return lista
+
+for line in läsa_fil(): # Printar ut antalet studenter som finns med i den skapade listan
+    print("Namn: " + line[0] + " " + line[1] + " Personnummer: " + line[2])
